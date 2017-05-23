51af31d449209a336d9e6cf736eeb36c93569e51
Colin Key <colin.m.key@gmail.com>
2017-05-22 22:33:09 -0600

    UI partially implemented, updated version.

--- app.js
+++ app.js
@@ -26,18 +26,13 @@ const client = new tmi.client(options)
 let win
 
 function createWindow () {
-  // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({width: 800, height: 600, autoHideMenuBar: true, title: "Carlton"})
 
-  // and load the index.html of the app.
   win.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
   }))
-
-  // Open the DevTools.
-  win.webContents.openDevTools()
 
   // Emitted when the window is closed.
   win.on('closed', () => {
@@ -48,23 +43,15 @@ function createWindow () {
   })
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
-// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (win === null) {
     createWindow()
   }
@@ -134,4 +121,11 @@ function callAHK(path) {
     })
 }
 
-
+function calibrateScenes() {
+    let child = exec('start iniwriter.ahk', (error, stdout, stderr) => {
+        if (error) {
+            throw error
+        }
+        console.log(stdout)
+    })
+}
