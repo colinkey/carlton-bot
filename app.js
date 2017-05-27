23a942be2a365e7774607d8c8dd9f87a1cbef875
Colin Key <Colin@MacBook.local>
2017-05-27 10:53:47 -0600

    Added bot.js to handle bot, app.js for electron. Fixed undefined app problem in electron render.

--- app.js
+++ app.js
@@ -1,131 +1,55 @@
-const tmi = require('tmi.js')
-const exec = require('child_process').exec
-const scenes = require('./activeScenes.js')
-const creds = require('./.creds.json')
-const social = require('./social.js')
-const { app, BrowserWindow } = require('electron')
+const electron = require('electron')
+const app = electron.app
+const BrowserWindow = electron.BrowserWindow
 const path = require('path')
 const url = require('url')
 
-const options = {
-    options: {
-        debug: true
-    },
-    connection: {
-        reconnect: true
-    },
-    identity: {
-        username: creds.twitch.username,
-        password: creds.twitch.token
-    },
-    channels:[creds.twitch.channel]
-}
-
-const client = new tmi.client(options)
-
-let win
+let mainWindow
 
 function createWindow () {
-  win = new BrowserWindow({width: 800, height: 600, autoHideMenuBar: true, title: "Carlton"})
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 800, height: 600})
 
-  win.loadURL(url.format({
+  // and load the index.html of the app.
+  mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'index.html'),
     protocol: 'file:',
     slashes: true
   }))
 
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
   // Emitted when the window is closed.
-  win.on('closed', () => {
+  mainWindow.on('closed', function () {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    win = null
+    mainWindow = null
   })
 }
 
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow)
 
-app.on('window-all-closed', () => {
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
 
-app.on('activate', () => {
-  if (win === null) {
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
     createWindow()
   }
 })
 
-client.connect()
-
-client.on('connected', function(address, port) {
-    client.action(creds.twitch.channel, 'Ready to serve.')
-})
-
-client.on('chat', function(channel, userstate, message, self) {
-    switch(message) {
-        case '!morework':
-        client.action(channel, 'Me not dat kind of orc')
-        break
-
-        case '!twitter':
-        client.action(channel, social.twitter)
-        break
-
-        case '!github':
-        client.action(channel, social.github)
-        break
-
-        case scenes.Scene1.chatCommand:
-        client.action(channel, AHKSwtichScene(scenes.Scene1))
-        break
-
-        case scenes.Scene2.chatCommand:
-        client.action(channel, AHKSwtichScene(scenes.Scene2))
-        break
-
-        case scenes.Scene3.chatCommand:
-        client.action(channel, AHKSwtichScene(scenes.Scene3))
-        break
-
-    }
-})
-
-function AHKSwtichScene(obj) {
-    if (obj.active == false) {
-        return obj.label + ' is not active or available for scene switching'
-    } else {
-        switch(obj.label) {
-            case 'Jake':
-            callAHK('start scene-switch-' + obj.label + '.ahk')
-            return 'Switched to ' + obj.label
-
-            case 'Benjamin':
-            callAHK('start scene-switch-' + obj.label + '.ahk')
-            return 'Switched to ' + obj.label
-            
-            case 'Greg':
-            callAHK('start scene-switch-' + obj.label + '.ahk')
-            return 'Switched to ' + obj.label
-        }
-    }
-}
-
-function callAHK(path) {
-    let child = exec(path, (error, stdout, stderr) => {
-        if (error) {
-            throw error
-        }
-        console.log(stdout)
-    })
-}
-
-function calibrateScenes() {
-    let child = exec('start iniwriter.ahk', (error, stdout, stderr) => {
-        if (error) {
-            throw error
-        }
-        console.log(stdout)
-    })
-}
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
